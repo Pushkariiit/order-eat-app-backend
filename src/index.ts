74e929d48b928f270887be47da2c62b050faa0e7
Pushkar Choudhary <pushkariiitv@gmail.com>
2024-05-31 03:29:55 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { Response, Request } from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import mongoose from 'mongoose';
+import myUserRoute from "./routes/MyUserRoute"
+
+
+mongoose.connect(process.env.MONGODB_URL as string).then(() => console.log("Connected to DB"));
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/healthy", async (req: Request, res: Response) => {
+    res.send({ message: "Health OK!" });
+})
+app.use("/api/my/user", myUserRoute);
+
+app.listen(7000, () => {
+    console.log("server started on port 7000");
+})

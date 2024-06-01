8d49ea9e52d6d95534536cc94b2aef212d8b671e
Pushkar Choudhary <pushkariiitv@gmail.com>
2024-06-01 18:40:02 +0530

    adding restaurant feature

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,17 @@ import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import myUserRoute from "./routes/MyUserRoute"
-
+import {v2 as cloudinary} from "cloudinary";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
 
 mongoose.connect(process.env.MONGODB_URL as string).then(() => console.log("Connected to DB"));
+
+cloudinary.config({
+    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+    api_key : process.env.CLOUDINARY_API_KEY,
+    api_secret : process.env.CLOUDINARY_API_SECRET
+});
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -14,7 +22,9 @@ app.get("/healthy", async (req: Request, res: Response) => {
     res.send({ message: "Health OK!" });
 })
 app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
-app.listen(7000, () => {
-    console.log("server started on port 7000");
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`);
 })
